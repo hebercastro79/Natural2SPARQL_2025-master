4c87bcf2d2118d5b7dd1fe2583dc1d58b0d6c234
Heber Castro <heber.castro@usp.br>
2025-06-30 13:37:11 -0300

    Correções

---
+++ src/main/java/com/example/Programa_heber/ProgramaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Programa_heber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramaHeberApplication.class, args);
+    }
+
+}
